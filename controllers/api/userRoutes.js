0287638029d40017190511ae32e961c2176e716f
ckim812 <109961170+ckim812@users.noreply.github.com>
2023-01-09 21:40:53 -0600

    worked on routing

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,12 +3,12 @@ const { User } = require('../../models');
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where: { username: req.body.username } });
 
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
@@ -17,7 +17,7 @@ router.post('/login', async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
-  res.render('login');
-});
+// router.get('/', (req, res) => {
+//   res.render('homepage');
+// });
 
 router.get('/', withAuth, async (req, res) => {
   try {
