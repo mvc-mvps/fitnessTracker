03620f031784652eab59b656fc781fdf7039107e
mmm <margaret.m.matson@gmail.com>
2023-01-10 19:55:00 -0600

    added models and added nutritionRoutes file with comment about where to fetch API data

---
+++ controllers/api/nutritionRoutes.js
@@ -0,0 +1,1 @@
+//do the fetch from the API in the nutrition routes controller page
--- models/index.js
+++ models/index.js
@@ -1,3 +1,38 @@
 const User = require('./User');
+const Exercise = require('./Exercise');
+const Nutrition = require('./Nutrition');
+const Planner = require('./Planner');
 
-module.exports = { User };
+User.hasMany(Exercise, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Exercise.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Nutrition, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Nutrition.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Planner.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Planner.hasMany(Nutrition, {
+    foreignKey: 'planner_id',
+    onDelete: 'CASCADE'
+});
+
+Planner.hasMany(Exercise, {
+    foreignKey: 'planner_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, Exercise, Nutrition, Planner };
