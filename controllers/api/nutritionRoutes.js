4b2cb4679ce1ef8d276c7aa91e2c8c4f9e17f561
ckim812 <109961170+ckim812@users.noreply.github.com>
2023-01-12 20:59:59 -0600

    working on showing results from nutrition API

--- controllers/api/nutritionRoutes.js
+++ controllers/api/nutritionRoutes.js
@@ -2,6 +2,9 @@
 
 const { Nutrition } = require('../../models');
 const router = require('express').Router();
+const axios = require('axios');
+const path = require('path');
+const express = require('express');
 
 // const displayBrand = document.getElementById('brand-display');
 // const displayCalories = document.getElementById('calorie-display');
@@ -10,46 +13,64 @@ const router = require('express').Router();
 
 // base inputs for request from api
 const requestUrl = 'https://api.nutritionix.com/v1_1/search/';
-const searchInput = 'mcdonalds';
+const searchInput = document.querySelector('#foodInput').value.trim();
 const resultParams =
   '?results=0:20&fields=item_name,brand_name,item_id,nf_calories,nf_protein';
-const appIdAndKey = '&appId=E77012d9&appKey=92d7491ebf927740552c294741b0472a-';
+const appIdAndKey = process.env.APP_ID_KEY;
 
 // //   const username = document.querySelector('#email-login').value.trim();
-// // const url = "https://api.nutritionix.com/v1_1/search/mcdonalds?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=E77012d9&appKey=92d7491ebf927740552c294741b0472a-"
 
 // let query = document.querySelector("#foodInput").value.trim();
 
 let request = `${requestUrl}${searchInput}${resultParams}${appIdAndKey}`;
 
-const nutritionData = () => {
-  console.log(request);
-  fetch(request)
-    .then(function (res) {
-      return res.json();
+const getNutritionDataa = () => {
+  axios
+    .get(request)
+    .then((response) => {
+      // Code for handling the response
+      console.log(response.data.hits[0].fields.brand_name);
+      data = response.data.hits[0];
+      express.use('/', function(req, res, next){
+        res.send({"name":"GeeksforGeeks"});
+        next();
+     });
     })
-    .then(function (data) {
-      console.log(data);
-      const brand = data.hits[0].fields.brand_name;
-      const calories = data.hits[0].fields.nf_calories;
-      console.log(brand);
-      console.log(calories);
+    .catch((error) => {
+      // Code for handling the error
+      console.error(error);
     });
 };
-nutritionData(request);
 
-// router.post('/', async (req, res) => {
-//     try {
-//       const nutritionData = await Nutrition.create({
-//       brand: req.body.hits[0].fields.brand_name,
-//       calories: req.body.hits[0].fields.nf_calories,
+// const getNutritionData = () => {
+//   console.log(request);
+//   fetch(request)
+//     .then(function (res) {
+//       return res.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       const brand = data.hits[0].fields.brand_name;
+//       const calories = data.hits[0].fields.nf_calories;
+//       console.log(brand);
+//       console.log(calories);
 //     });
-//     // if the dish is successfully created, the new response will be returned as json
-//     res.status(200).json(nutritionData)
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-//   });
+// };
+// getNutritionData(request);
+
+router.post('/', async (req, res) => {
+  try {
+    getNutritionDataa();
+    const getNutritionData = await Nutrition.create({
+      name: req.body.hits[0].fields.item_name,
+      calories: req.body.hits[0].fields.nf_calories,
+    });
+    // if the dish is successfully created, the new response will be returned as json
+    res.status(200).json(getNutritionData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // router.post('/', async (req, res) => {
 //   res.render(console.log(foodSearch(request)));
@@ -66,7 +87,5 @@ nutritionData(request);
 //     res.status(500).json(err);
 //   }
 // });
-
-
 
 module.exports = router;
