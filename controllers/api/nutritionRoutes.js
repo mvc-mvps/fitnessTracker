b2b9bd21ddc26843d8818f1e2afe3626aba7f0e0
ckim812 <109961170+ckim812@users.noreply.github.com>
2023-01-13 23:05:59 -0600

    got fetch working from nutritionix; got routing working for nutrition.js

--- controllers/api/nutritionRoutes.js
+++ controllers/api/nutritionRoutes.js
@@ -6,86 +6,22 @@ const axios = require('axios');
 const path = require('path');
 const express = require('express');
 
-// const displayBrand = document.getElementById('brand-display');
-// const displayCalories = document.getElementById('calorie-display');
-// const displayProtein = document.getElementById('protein-display');
-// const searchButton = document.getElementById('search-button');
-
 // base inputs for request from api
-// const requestUrl = 'https://api.nutritionix.com/v1_1/search/';
-// // const searchInput = document.querySelector('#foodInput').value.trim();
-// const resultParams =
-//   '?results=0:20&fields=item_name,brand_name,item_id,nf_calories,nf_protein';
-// const appIdAndKey = process.env.APP_ID_KEY;
+const requestUrl = 'https://api.nutritionix.com/v1_1/search/';
+const resultParams =
+  '?results=0:20&fields=item_name,brand_name,item_id,nf_calories,nf_protein';
+const appIdAndKey = process.env.APP_ID_KEY;
 
-// //   const username = document.querySelector('#email-login').value.trim();
-
-// let query = document.querySelector("#foodInput").value.trim();
-
-// let request = `${requestUrl}${searchInput}${resultParams}${appIdAndKey}`;
-
-const getNutritionDataa = () => {
-  axios
-    .get(request)
-    .then((response) => {
-      // Code for handling the response
-      console.log(response.data.hits[0].fields.brand_name);
-      data = response.data.hits[0];
-      express.use('/', function(req, res, next){
-        res.send({"name":"GeeksforGeeks"});
-        next();
-     });
-    })
-    .catch((error) => {
-      // Code for handling the error
-      console.error(error);
-    });
-};
-
-// const getNutritionData = () => {
-//   console.log(request);
-//   fetch(request)
-//     .then(function (res) {
-//       return res.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       const brand = data.hits[0].fields.brand_name;
-//       const calories = data.hits[0].fields.nf_calories;
-//       console.log(brand);
-//       console.log(calories);
-//     });
-// };
-// getNutritionData(request);
-
-router.post('/', async (req, res) => {
-  try {
-    getNutritionDataa();
-    const getNutritionData = await Nutrition.create({
-      name: req.body.hits[0].fields.item_name,
-      calories: req.body.hits[0].fields.nf_calories,
-    });
-    // if the dish is successfully created, the new response will be returned as json
-    res.status(200).json(getNutritionData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+router.get('/', async (req, res) => {
+  let request = `${requestUrl}${nutritionSearchInput}${resultParams}${appIdAndKey}`;
+  axios.get(request).then((response) => {
+    // Code for handling the response
+    console.log('data: ', response.data);
+    let data = response.data.hits[0];
+    res.render('nutrition', (apiData = data));
+  });
+  // if the dish is successfully created, the new response will be returned as json
+  res.status(200).json();
 });
-
-// router.post('/', async (req, res) => {
-//   res.render(console.log(foodSearch(request)));
-//   res.render('console.log(foodSearch(request))');
-// });
-
-// router.get('/', async (req, res) => {
-//   try {
-//     const plannerData = await Planner.findAll({
-//       include: [{ model: Nutrition }, { model: Exercise }],
-//     });
-//     res.status(200).json(plannerData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 module.exports = router;
--- public/js/nutrition.js
+++ public/js/nutrition.js
@@ -1,51 +1,39 @@
-// const requestUrl = 'https://api.nutritionix.com/v1_1/search/';
-// const searchInput = document.getElementById('foodInput');
-// const resultParams = '?results=0:20&fields=item_name,brand_name,item_id,nf_calories,nf_protein';
-// const appIdAndKey = process.env.APP_ID_KEY;
 const searchButton = document.getElementById('search-button');
-
-
-// const nutritionData = () => {
-// let request = `${requestUrl}${searchInput}${resultParams}${appIdAndKey}`
-//   console.log(request);
-//   fetch(request)
-//     .then(function (res) {
-//       return res.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       for(i = 0; i < data.hit.length; i++) {
-//       const brand = data.hits[i].fields.brand_name;
-//       const calories = data.hits[i].fields.nf_calories;
-//       const protein = data.hits[i].fields.nf_protein;
-//       console.log(brand);
-//       console.log(calories);
-//       console.log(protein);
-//       }
-//     });
-// };
-
-// document
-//   .querySelector('#foodInput-form')
-//   .addEventListener('submit', function() {
-//     nutritionData();
-//   });
+const nutritionSearchInput = document.querySelector('#food-input').value.trim();
 
 const postNutrition = async () => {
-  const response = await fetch('/api/nutrition', {
-    method: 'POST',
-    // headers: { 'Content-Type': 'application/json' },
-  });
+  const response = await fetch('/api/nutrition').then((data) => data.json());
 
   if (response.ok) {
-    alert("Nutrition Data pull successful");
+    alert('Nutrition Data pull successful');
   } else {
     alert(response.statusText);
   }
 };
 
-searchButton.addEventListener('click', function(event) {
+searchButton.addEventListener('click', function (event) {
   event.preventDefault();
-  console.log('testing');
+  console.log("search input: ", nutritionSearchInput);
   postNutrition();
 });
+
+// const nutritionSearchHandler = async (buttonClick) => {
+//   buttonClick.preventDefault();
+//   const nutritionSearchInput = document.getElementById('#food-input').value.trim();
+//   if (nutritionSearchInput) {
+//     const res = await fetch('/api/nutrition', {
+//       method: 'POST',
+//       body: JSON.stringify({ nutritionSearchInput }),
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+//     if (res.ok) {
+//       console.log('success');
+//     } else {
+//       console.log('keep trying');
+//     }
+//   }
+// };
+
+// document
+//   .querySelector('#food-input')
+//   .addEventListener('click', nutritionSearchHandler);
