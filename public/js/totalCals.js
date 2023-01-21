77f9598d115f5cec6b2c0da0830bea15ead5cb81
ckim812 <109961170+ckim812@users.noreply.github.com>
2023-01-21 13:43:05 -0600

    added progress bar, updated status alerts, changed app name, worked on css

--- public/js/totalCals.js
+++ public/js/totalCals.js
@@ -1,14 +1,28 @@
 const display = () => {
-    const array = [];
-    const calorieData = document.querySelectorAll('.calorieData');
-    for(i = 0;i < calorieData.length;i++) {
-      array.push(calorieData[i].innerHTML);
-    }
-    const arrayInt = array.map(Number);
-    const total = arrayInt.reduce(function(a, b) {return a + b; }, 0);
-    if(total > 0){
-    alert(`You have logged ${total} calories, Great Job!`);
-    }
+  const arrayC = [];
+  const arrayP = [];
+  const calorieData = document.querySelectorAll('.calorieData');
+  const proteinData = document.querySelectorAll('.proteinData');
+  for (i = 0; i < calorieData.length; i++) {
+    arrayC.push(calorieData[i].innerHTML);
   }
-  
-  window.addEventListener('load', display);
+  for (i = 0; i < proteinData.length; i++) {
+    arrayP.push(proteinData[i].innerHTML);
+  }
+  const arrayIntC = arrayC.map(Number);
+  const totalC = arrayIntC.reduce(function (a, b) {
+    return a + b;
+  }, 0);
+  const arrayIntP = arrayP.map(Number);
+  const totalP = arrayIntP.reduce(function (a, b) {
+    return a + b;
+  }, 0);
+  if (totalC > 0 || totalP > 0) {
+    document.querySelector(
+      '.alert'
+    ).innerHTML = `You have logged ${totalC} calories and ${totalP}g of protein, Great Job!`;
+    // alert(`You have logged ${total} calories, Great Job!`);
+  }
+};
+
+window.addEventListener('load', display);
