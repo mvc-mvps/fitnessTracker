454f9bf999ca4bc9a039e153d5787fe62d51d61f
ckim812 <109961170+ckim812@users.noreply.github.com>
2023-01-21 10:28:46 -0600

    worked on routes and handlebars for displaying data on homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { request } = require('express');
-const { User, Planner } = require('../models');
+const { User, Planner, Nutrition } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
@@ -38,7 +38,42 @@ router.get('/signup', (req, res) => {
 });
 
 router.get('/homepage', async (req, res) => {
-  res.render('homepage');
+  await Nutrition.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ['id', 'name', 'protein', 'calories', 'serving'],
+  })
+    .then((nutritionData) => {
+      const nutritionitems = nutritionData.map((nutritionitem) =>
+        nutritionitem.get({ plain: true })
+      );
+      // console.log(data);
+      res.render('homepage', { data1: nutritionitems });
+      // res.json(nutritionitems);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
+    await Planner.findAll({
+      where: {
+        user_id: req.session.user_id
+    },
+      attributes: ['id', 'date', 'type', 'goal', 'completed'],
+    })
+      .then((plannerData) => {
+        const planneritems = plannerData.map((planneritem) =>
+          planneritem.get({ plain: true })
+        );
+        res.send({ data2: planneritems });
+        // res.json(planneritems);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 router.get('/nutrition', (req, res) => {
@@ -50,7 +85,8 @@ router.get('/exercise-homepage', (req, res) => {
 });
 
 router.get('/nutrition-homepage', (req, res) => {
-  res.render('nutrition-homepage')});
+  res.render('nutrition-homepage');
+});
 
 router.get('/addexercise', (req, res) => {
   res.render('addexercise');
@@ -67,7 +103,5 @@ router.get('/addfood', (req, res) => {
 router.get('/updatefood/:id', (req, res) => {
   res.render('updatefood');
 });
-
-
 
 module.exports = router;
