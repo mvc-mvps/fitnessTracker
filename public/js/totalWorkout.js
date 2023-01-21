17224d8aa97ad71930e92fddc5a53258bfd0a4a0
nic <nrhoda14@gmail.com>
2023-01-21 09:45:39 -0600

    made exercise homepage more interactive with goal completion logic

--- public/js/totalWorkout.js
+++ public/js/totalWorkout.js
@@ -1,14 +1,40 @@
 const display = () => {
-    const idData = document.querySelectorAll('.idData');
-    const display = idData.length.toString();
+  const idData = document.querySelectorAll('.idData');
+  const display = idData.length.toString();
     console.log(`you have planned/logged ${display} workouts, Keep it up!`)
     if(display == 1) {
-    alert(`You have planned/logged ${display} workout, Keep it up!`)
+    console.log(`You have planned/logged ${display} workout, Keep it up!`)
     } else if(display > 1){
-      alert(`You have logged/planned ${display} workouts, Keep it up!`)
+      console.log(`You have logged/planned ${display} workouts, Keep it up!`)
     } else {
       return;
     }
   }
+const goals = () => {
+  const goalArray = [];
+  const completedArray = [];
+  const goalData = document.querySelectorAll('.goalData');
+  const completedData = document.querySelectorAll('.completedData');
+  for(i = 0; i < goalData.length; i++) {
+    goalArray.push(goalData[i].innerHTML);
+  }
+  for(i = 0; i < completedData.length; i++) {
+    completedArray.push(completedData[i].innerHTML);
+  }
+  const goalArrayInt = goalArray.map(Number);
+  const completedArrayInt = completedArray.map(Number);
+  const goalTotal = goalArrayInt.reduce(function(a, b) {return a + b; }, 0);
+  const completedTotal = completedArrayInt.reduce(function(a, b) {return a + b; }, 0);
+  let value = goalTotal - completedTotal;
+  if(value > 0) {
+    alert(`Almost there! Only ${value} reps/minutes left until you hit your goals`);
+  } else if (value < 0) {
+    alert(`You crushed your workout goals with ${value} extra!`);
+  } else {
+    alert('Great job! You have completed all of your set goals!');
+  }
   
-  window.addEventListener('load', display);
+}
+
+window.addEventListener('load', display);
+window.addEventListener('load', goals);
